b6e1ade25bdc3eefbf7ea0c56655f79ac712c514
yang fanou <1539214893@qq.com>
2022-08-12 01:17:41 +0800

    基本骨架

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div>name: {{ name }}</div>
+<div>grade: {{grade}}</div>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name: "App",
+  setup: () => {
+    let name = ref("");
+    let grade = ref("");
+    $.ajax({
+      url:"http://localhost:3000/pk/getInfo/",
+      type:"get",
+      success: resp => {
+        name.value = resp.name;
+        grade.value = resp.grade;
+      }
+    });
+    return {
+      name,
+      grade
+    }
+  }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body{
+  background-image: url(@/assets/background.png);
+  background-size: cover;
 }
 </style>
