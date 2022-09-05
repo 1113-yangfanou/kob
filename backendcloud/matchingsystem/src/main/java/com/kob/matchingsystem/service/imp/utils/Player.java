36d2cabd75c1b07687589d64fe1fe4dcc84f12e6
yang fanou <1539214893@qq.com>
2022-09-05 22:14:29 +0800

    实现微服务--匹配系统

---
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/service/imp/utils/Player.java
@@ -0,0 +1,14 @@
+package com.kob.matchingsystem.service.imp.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+    private Integer userId;
+    private Integer rating;
+    private Integer waitingTime;
+}
--- web/src/App.vue
+++ web/src/App.vue
@@ -11,7 +11,38 @@ import "bootstrap/dist/js/bootstrap"
 
 export default {
 
-    components: { NavBar }
+    components: { NavBar },
+    setup() {
+      // $.ajax({
+      //   url: "http://127.0.0.1:3000/user/account/token/",
+      //   type: "post",
+      //   data: {
+      //     username: 'yangfanou',
+      //     password: '123456'
+      //   },
+      //   success(resp) {
+      //     console.log(resp);
+      //   },
+      //   error(resp) {
+      //     console.log(resp);
+      //   }
+      // })
+      // $.ajax({
+      //   url: "http://127.0.0.1:3000/user/account/info/",
+      //   type: "get",
+      //   headers: {
+      //     Authorization:"Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiMzI4NzY5OTc2M2Y0ZTdjOTVhODRjYWY4MDBjOTZkYSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY2MjA5NTU1NCwiZXhwIjoxNjYzMzA1MTU0fQ.ouMTsWNhwr_SUyV3Ve8bnYwTRYJeSgZjkMr2lTHtniM"
+
+      //   },
+      //   success(resp) {
+      //     console.log(resp);
+      //   },
+      //   error(resp) {
+      //     console.log(resp);
+      //   }
+      // })
+      
+    }
 }
 
 </script>
